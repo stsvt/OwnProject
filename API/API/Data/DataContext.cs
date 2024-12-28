088cd3121d494c630683ebd6956469f0652781fe
stsv.t <taras.stasiv3@gmail.com>
2024-12-28 23:02:03 +0200

    Initial commit

---
+++ API/API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+// dotnet ef migrations add --project API\API.csproj --startup-project API\API.csproj --context API.Data.DataContext --configuration Debug first_migration --output-dir Data\Migrations
+// dotnet ef database update --project API\API.csproj --startup-project API\API.csproj --context API.Data.DataContext --configuration Debug
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> UsersData { get; set; }
+    }
+}
---
+++ API/API/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string LastName { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+    }
+}
