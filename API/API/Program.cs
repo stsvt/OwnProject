2bde115f7b750969b7723de80be9d902304ef19d
stsv.t <taras.stasiv3@gmail.com>
2025-01-03 14:38:26 +0200

    implement password hasher

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,8 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Data.Common;
+using API.Data.Services;
+using API.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IPasswordHasherService, PasswordHasherService>();
 builder.Services.AddLogging(options =>
 {
     options.AddConsole();
--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe40d578043dd6d8eff326a080ca65e731820331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f916bf49bd33d5ac610e75694a438ae32443e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
